19a2593eb035b395361e363b56cb1a55b157b132
Chandresh Pant <cp@cpant.in>
2017-05-21 15:51:04 +0530

    Implemented search without relevance * Better logic using regular expressions * Added more Tests

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,43 +1,42 @@
 class Search
-
+  SPECIAL_CHARS = %w(" -)
   attr_accessor :keywords, :exact_match
 
   def initialize(keywords)
     # Original keywords entered by user
     @keywords           = keywords
     # Copy of keywords lowercased for further processing
-    @processed_keywords = keywords.downcase
+    @processed_keywords = keywords
+                              .downcase
+                              .gsub(/(?=\S)"/, ' "') # Add space before quote that are directly after a character
+                              .gsub(/"(?=\S)/, '" ') # Add space after quote that are directly before a character
 
-    @tokens             = @processed_keywords.split
-    @negative_tokens    = @tokens.select { |token| token.start_with?('-') }.map { |token| token[1..-1] }
-    @tokens             = @tokens.reject { |token| token.start_with?('-') }
-    # The keywords string starts and ends with "
-    @exact_match        = /^".+"$/.match(keywords)
-    if @exact_match
-      # if it's an exact match, lets remove " from begineing & end
-      @processed_keywords = @processed_keywords[1..-2]
-    end
+    @tokens          = []
+    @negative_tokens = []
+
+    # call tokenize method to populate @tokens & @negative_tokens
+    tokenize
   end
 
   def results
-    if @exact_match
-      # if it's an exact match we will match it exactly with name, type and designed_by
-      exact_search(data)
-    else
-      match_tokens(remove_negative_tokens(data))
-    end
-
+    match_tokens(remove_negative_tokens(data))
   end
 
   private
 
-  def exact_search(data)
-    data.select do |row|
-      row.name.downcase == @processed_keywords || row.type.downcase == @processed_keywords || row.designed_by.downcase == @processed_keywords
-    end
+  def tokenize
+    @tokens = @processed_keywords
+                  .scan(/(?:[^ \t\r\n\f"]|"[^"]*")+/) # Non-whitespace characters or sentences within quotes
+                  .map { |token| token.gsub('"', '').strip } # Remove quotes and extra space from token beginning & end
+                  .reject(&:blank?) # remove any tokens with only whitespace
+
+    @negative_tokens = @tokens.select { |token| token.start_with?('-') }
+                           .map { |token| token.gsub('-', '') }
+    @tokens          = @tokens.reject { |token| token.start_with?('-') }
+
   end
 
-  # This method takes in data in an array and returns an array containing all results with tokens
+# This method takes in data in an array and returns an array containing all results with tokens
   def match_tokens(data)
     @tokens.each do |token|
       data = data.select do |row|
@@ -47,7 +46,7 @@ class Search
     data
   end
 
-  # This method takes in data in an array and returns an array removing all results containing negative tokens
+# This method takes in data in an array and returns an array removing all results containing negative tokens
   def remove_negative_tokens(data)
     @negative_tokens.each do |negative_token|
       data = data.reject do |row|
--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
 
 class SearchesTest < ActionController::TestCase
-  test "search#results should return correct records" do
+  test 'search#results should return correct records' do
     # Keyword search
     @search = Search.new('Java')
-    assert_includes(@search.results.map{|row| row.name}, "Java")
-    assert_includes(@search.results.map{|row| row.name}, "JavaScript")
+    assert_includes(@search.results.map { |row| row.name }, 'Java')
+    assert_includes(@search.results.map { |row| row.name }, 'JavaScript')
+    @search = Search.new('Scripting Microsoft')
+    assert_includes(@search.results.map { |row| row.name }, 'JScript')
+    assert_includes(@search.results.map { |row| row.name }, 'VBScript')
+    assert_includes(@search.results.map { |row| row.name }, 'Windows PowerShell')
     # Absolute search
-    @search = Search.new('"Java"')
-    assert_includes(@search.results.map{|row| row.name}, "Java")
-    refute_includes(@search.results.map{|row| row.name}, "JavaScript")
+    @search = Search.new('Interpreted "Thomas Eugene"')
+    assert_includes(@search.results.map { |row| row.name }, 'BASIC')
+    refute_includes(@search.results.map { |row| row.name }, 'Haskell')
     # Negative search
     @search = Search.new('-Java')
-    refute_includes(@search.results.map{|row| row.name}, "Java")
-    refute_includes(@search.results.map{|row| row.name}, "JavaScript")
+    refute_includes(@search.results.map { |row| row.name }, 'Java')
+    refute_includes(@search.results.map { |row| row.name }, 'JavaScript')
+
+    @search = Search.new('john -array')
+    assert_includes(@search.results.map { |row| row.name }, 'BASIC')
+    assert_includes(@search.results.map { |row| row.name }, 'Haskell')
+    assert_includes(@search.results.map { |row| row.name }, 'Lisp')
+    assert_includes(@search.results.map { |row| row.name }, 'S-Lang')
+    refute_includes(@search.results.map { |row| row.name }, 'Chapel')
+    refute_includes(@search.results.map { |row| row.name }, 'Fortran')
+    refute_includes(@search.results.map { |row| row.name }, 'S')
   end
 
 end
